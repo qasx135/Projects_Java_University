a5f9d9fee42363b5993062d813a715faaf59ca19
qasx135 <qasx135@gmail.com>
2023-09-05 22:09:07 +0300

    Created class Shop and Computer for task 4

---
+++ src/LAB2/number4/Computer.java
@@ -0,0 +1,10 @@
+package LAB2.number4;
+
+public class Computer {
+    String name;
+    int number;
+    public Computer(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+}
--- src/LAB2/number4/Shop.java
+++ src/LAB2/number4/Shop.java
@@ -1,4 +1,32 @@
 package LAB2.number4;
 
-public class Shop {
+public class Shop implements ShopInterface{
+
+    private Computer[] computers;
+    private int count = 5;
+    private int countComputers = 0;
+
+    public Shop(int count) {
+        computers = new Computer[count];
+    }
+
+    @Override
+    public void addPc(String namePc, int numberPc) {
+        countComputers += 1;
+        if (countComputers == count) {
+            Computer[] newComputers = new Computer[count*2];
+            System.arraycopy(computers, 0, newComputers, 0, computers.length);
+            computers = newComputers;
+        }
+    }
+
+    @Override
+    public String findPcByName(String namePc) {
+        return null;
+    }
+
+    @Override
+    public String findPcByNumber(int numberPc) {
+        return null;
+    }
 }
