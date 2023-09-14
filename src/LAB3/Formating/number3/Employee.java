5e1650ab59b7c805f745193622942f8d7353ae18
qasx135 <qasx135@gmail.com>
2023-09-14 09:42:27 +0300

    Started do number 3 from formatting

---
+++ src/LAB3/Formating/number3/Employee.java
@@ -0,0 +1,6 @@
+package LAB3.Formating.number3;
+
+public class Employee {
+    public String fullname;
+    public  Double salary
+}
---
+++ src/LAB3/Formating/number3/Report.java
@@ -0,0 +1,7 @@
+package LAB3.Formating.number3;
+
+public class Report {
+    static String info(Employee em) {
+
+    }
+}
